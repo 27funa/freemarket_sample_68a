222a948749e629e3a0cf87294b0a19c2cfb8b592
yuto-osawa6 <pooh.atgmw628@gmail.com>
2020-05-14 21:26:14 +0900

    post_function

--- app/controllers/osawas_controller.rb
+++ app/controllers/osawas_controller.rb
@@ -1,2 +1,4 @@
 class OsawasController < ApplicationController
+  def index
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_06_020819) do
+ActiveRecord::Schema.define(version: 2020_05_14_094519) do
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "image"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_images_on_post_id"
+  end
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "item_name", null: false
+    t.text "description", null: false
+    t.boolean "sales_status", default: true, null: false
+    t.string "brand", null: false
+    t.string "size", null: false
+    t.string "condition", null: false
+    t.integer "price", null: false
+    t.string "shipping_area", null: false
+    t.string "arrival_days", null: false
+    t.string "postage_payment", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
