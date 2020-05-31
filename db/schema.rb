84da628fe403feb48c4398fc9783a11348a17863
T-SGR3u <vinlandsaga114@gamil.com>
2020-05-31 16:10:51 +0900

    ancestryに関係するマイグレーションファイルの順番修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_23_082323) do
+ActiveRecord::Schema.define(version: 2020_05_31_070215) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
