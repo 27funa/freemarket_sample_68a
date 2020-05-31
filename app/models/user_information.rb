406ef4de0f555467f9e19ffb5793749c7c12cf78
T-SGR3u <vinlandsaga114@gamil.com>
2020-06-01 01:31:49 +0900

    バリデーション設定

--- app/models/user_information.rb
+++ app/models/user_information.rb
@@ -1,3 +1,7 @@
 class UserInformation < ApplicationRecord
   belongs_to :user
+
+  validates :family_name,:first_name, presence: true, format: {with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/}
+  validates :family_name_kana,:first_name_kana, presence: true, format: {with: /\A[ぁ-んァ-ヶー－]+\z/}
+  validates :birth,:postal_code,:state,:city,:address_line_1, presence: true
 end
