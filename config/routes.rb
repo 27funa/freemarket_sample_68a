5ced345af747b01a0d4bce5b979d3fe371828ba4
27funa <k_funasaki@icloud.com>
2020-06-17 19:42:10 +0900

    add buy_credit contorller & form
    購入画面からクレジット登録を行う。

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,18 @@ Rails.application.routes.draw do
   get 'logouts/index'
   root 'posts#index'
 
+
+
   resources :posts
+  resources :buys, only: [:index, :destroy]
   resources :profiles
   resources :user_informations
   resources :delivery_informations
   resources :osawas, only: :index
   resources :credits, except: [:show, :edit, :update]
   resources :categories, only: [:index, :new]
+  resources :buy_credit, except: [:show, :edit, :update]
+
 
   resources :images,only: [:create]
 end
